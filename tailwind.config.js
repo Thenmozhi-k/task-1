c43be3906306e4316e306b8475a4c62103a8e176
Thenmozhi <jothi552004@gmail.com>
2024-10-16 17:35:56 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,15 @@ export default {
       fontFamily: {
         timmy: ['Timmy', 'sans-serif'],
       },
+      width: {
+        '400': '400px',
+        '500': '500px',
+      },
+      height: {
+        '400': '400px',
+        '500': '500px',
+      },
     },
   },
   plugins: [],
 }
-
