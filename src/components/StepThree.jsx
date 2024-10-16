e8d31158a6ad75e128bd7b500b144b54a10f228e
Thenmozhi <jothi552004@gmail.com>
2024-10-16 20:27:27 +0530

    first commit

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -8,47 +8,46 @@ import step2 from '../assets/updated/step2.png'
 const StepThree = ({ onNavigate, onBack }) => {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
-    <div className="relative w-[500px] h-[500px] bg-[#161616] shadow-lg p-2 flex flex-col items-center">
+    <div className="relative md:w-[500px] md:h-[500px] sm:h-[350px] sm:w-[350px] bg-[#161616] shadow-lg p-2 flex flex-col items-center">
       <div
-        className="relative shadow-lg  p-6 flex flex-col justify-between"
+        className="relative shadow-lg md:w-[485px] md:h-[230px] sm:h-[160px] sm:w-[335px]  p-6 flex flex-col justify-between"
         style={{
           backgroundImage: `url(${first})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          width: '485px',
-          height: '230px',
+          
         }}
       >
-        <div className='ml-[330px]'>
-          <img src={buk} alt="" className='w-[70px] ml-12' />
+        <div className='md:ml-[330px] sm:ml-[200px]'>
+          <img src={buk} alt="" className='md:w-[70px] w-[50px] md:ml-12 sm:ml-[35px]' />
           
         </div>
 
         {/* content */}
-        <div className=' mt-48'>
+        <div className=' md:mt-48 sm:mt-32'>
           <div className='flex'>
           <div className='flex ml-[-15px]'>
               <div className='text-white flex '>
-                 <img src={step2} alt="" className='w-7 h-7'/>
-                 <p className='text-xs mt-2 ml-3'>Step 1</p>      
+                 <img src={step2} alt="" className='md:w-7 md:h-7 sm:w-5 sm:h-5'/>
+                 <p className='md:text-xs sm:text-[10px] md:mt-2 sm:mt-1 md:ml-3 sm:ml-2'>Step 1</p>      
               </div>
 
-              <div className=' bg-[#CA3F2A] h-[0.5px] w-[90px] mt-4 ml-3'></div>
+              <div className=' bg-[#CA3F2A] h-[0.5px] md:w-[90px] sm:w-[55px] md:mt-4 sm:mt-3 md:ml-3 sm:ml-2'></div>
           </div>
 
           <div className='flex ml-2'>
               <div className='text-white flex '>
-                 <img src={step2} alt="" className='w-7 h-7'/>
-                 <p className='text-xs mt-2 ml-3'>Step 2</p>      
+                 <img src={step2} alt="" className='md:w-7 md:h-7 sm:w-5 sm:h-5'/>
+                 <p className='md:text-xs sm:text-[10px] md:mt-2 sm:mt-1 md:ml-3 sm:ml-2'>Step 2</p>      
               </div>
 
-              <div className=' bg-[#CA3F2A] h-[0.5px] w-[90px] mt-4 ml-3'></div>
+              <div className=' bg-[#CA3F2A] h-[0.5px] md:w-[90px] sm:w-[55px] md:mt-4 sm:mt-3 md:ml-3 sm:ml-2'></div>
           </div>
 
           <div className='flex ml-2'>
               <div className='text-white flex '>
-                 <img src={step} alt="" className='w-7 h-7'/>
-                 <p className='text-xs mt-2 ml-3 '>Step 3</p>      
+                 <img src={step} alt="" className='md:w-7 md:h-7 sm:w-5 sm:h-5'/>
+                 <p className='md:text-xs sm:text-[10px] md:mt-2 sm:mt-1 md:ml-3 sm:ml-2'>Step 3</p>      
               </div>
 
               {/* <div className=' bg-[#CA3F2A] h-[0.5px] w-[80px] mt-4 ml-2'></div> */}
@@ -56,11 +55,11 @@ const StepThree = ({ onNavigate, onBack }) => {
           </div>
 
           {/* form */}
-          <div className="flex flex-col items-center  mt-10 "> 
-         <p className='text-center text-white text-sm font-light mb-5'>You’re purchasing Superior room at the  Park Plaza <br /> Westminster Bridge London  Park Plaza Westminster <br /> Bridge London  from 24-06-2024 to 24-06-2024 <br /> for USDC 447.90 for 2 guests.</p>
-          <div className='flex w-full items-center justify-center mt-7 '>
-              <img src={arrow} alt="" className='w-9 h-9 mr-4 cursor-pointer' onClick={onBack}/>
-          <button className="bg-[#CA3F2A] text-white px-[110px] py-1 rounded-md text-lg border-[#FFE3E3] border border-opacity-50   "
+          <div className="flex flex-col items-center  md:mt-9 sm:mt-7 "> 
+         <p className='text-center text-white md:text-sm sm:text-xs font-light md:mb-5 sm:mb-3'>You’re purchasing Superior room at the  Park Plaza <br /> Westminster Bridge London  Park Plaza Westminster <br /> Bridge London  from 24-06-2024 to 24-06-2024 <br /> for USDC 447.90 for 2 guests.</p>
+          <div className='flex w-full items-center justify-center md:mt-7 sm:mt-5 '>
+              <img src={arrow} alt="" className='md:w-9 md:h-9 sm:w-6 sm:h-6 mr-4 cursor-pointer' onClick={onBack}/>
+          <button className="bg-[#CA3F2A] sm:text-xs  text-white md:px-[110px] sm:px-[68px] md:py-1 sm:py-1 rounded-md md:text-lg border-[#FFE3E3] border border-opacity-50   "
            onClick={onNavigate}>
             Next
           </button>
