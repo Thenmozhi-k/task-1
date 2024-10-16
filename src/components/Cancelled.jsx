51822d7d4281a3de683db59a6970dd93266cc535
Thenmozhi <jothi552004@gmail.com>
2024-10-16 20:41:27 +0530

    first commit

--- src/components/Cancelled.jsx
+++ src/components/Cancelled.jsx
@@ -6,32 +6,31 @@ import error from '../assets/updated/error.png'
 const Cancelled = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-black">
-    <div className="relative w-[500px] h-[500px] bg-[#161616] shadow-lg p-2 flex flex-col items-center">
+    <div className="relative md:w-[500px] md:h-[500px] sm:h-[350px] sm:w-[350px] bg-[#161616] shadow-lg p-2 flex flex-col items-center">
       <div
-        className="relative shadow-lg  p-6 flex flex-col justify-between"
+        className="relative shadow-lg md:w-[485px] md:h-[230px] sm:h-[160px] sm:w-[335px] p-6 flex flex-col justify-between"
         style={{
           backgroundImage: `url(${first})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          width: '485px',
-          height: '230px',
+          
         }}
       >
-        <div className='ml-[330px]'>
-          <img src={buk} alt="" className='w-[70px] ml-12' />
+        <div className='md:ml-[330px] sm:ml-[200px]'>
+          <img src={buk} alt="" className='md:w-[70px] w-[50px] md:ml-12 sm:ml-[35px]' />
           
         </div>
 
         {/* content */}
-        <div className=' mt-48 items-center justify-center flex flex-col text-center'>
-         <img src={error} alt="" className='w-16 h-16 ' />
+        <div className=' md:mt-48 sm:mt-32 items-center justify-center flex flex-col text-center'>
+         <img src={error} alt="" className='md:w-16 md:h-16 sm:h-12 sm:w-12' />
        
-         <h2 className='text-white text-lg mt-5'>Oops! Booking resold. <br />
+         <h2 className='text-white md:text-lg sm:text-sm md:mt-5 sm:mt-3'>Oops! Booking resold. <br />
          Check our marketplace for more deals!</h2>
 
          
 
-         <button className="bg-[#CA3F2A] text-white px-[80px] mt-12 py-1 rounded-md text-lg border-[#FFE3E3] border border-opacity-50   "
+         <button className="bg-[#CA3F2A] text-white md:px-[40px] md:mt-8 sm:px-[30px] sm:mt-4 py-1 rounded-md md:text-lg sm:text-xs border-[#FFE3E3] border border-opacity-50   "
           >
           More Deals
           </button>
