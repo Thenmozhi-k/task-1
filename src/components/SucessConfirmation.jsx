3becc935d52a4c824e1b72542d3c03b68565b84b
vishalmet <vishalmet003@gmail.com>
2024-11-01 18:43:05 +0530

    redirect

--- src/components/SucessConfirmation.jsx
+++ src/components/SucessConfirmation.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import first from '../assets/updated/bg.png';
 import buk from '../assets/updated/buk.png';
 import tick from '../assets/updated/tick.png'
 
-const SucessConfirmation = () => {
+const SucessConfirmation = ({tokenID}) => {
+  const [transactionId, setTransactionId] = useState('');
+
+  useEffect(() => {
+    // Retrieve transaction ID from localStorage
+    const txId = localStorage.getItem('transactionId');
+    if (txId) {
+      setTransactionId(txId);
+    }
+  }, []);
+
+
+
+
   return (
     <div className="flex justify-center items-center h-screen bg-black">
     <div className="relative md:w-[500px] md:h-[500px] sm:h-[350px] sm:w-[350px] bg-[#161616] shadow-lg p-2 flex flex-col items-center">
@@ -28,15 +41,15 @@ const SucessConfirmation = () => {
          <h2 className='text-white md:text-lg sm:text-sm'>Congratulations! <br />
          You own this booking now.</h2>
 
-         {/* <div className='flex flex-col text-center '>
+         <div className='flex flex-col text-center '>
             <p className='text-[#CA3F2A] md:text-xs sm:text-[8px]'> Transaction id: </p>
-           <p className='md:text-[10px] sm:text-[6px] text-[#CACACA]'>0xbb9db37ae6a0b1cf4d09932b269255dbcc961228e572cc3ee526a397268aa243</p>
-         </div> */}
+           <p className='md:text-[10px] sm:text-[6px] text-[#CACACA]'>{transactionId || 'Pending...'}</p>
+         </div>
 
-         <button className="bg-[#CA3F2A] text-white md:px-[40px] md:mt-8 sm:px-[30px] sm:mt-4 py-1 rounded-md md:text-lg sm:text-xs border-[#FFE3E3] border border-opacity-50   "
+         <a href={`https://polygon.dassets.xyz/hotels/nft-details?nftId=${tokenID}`} className="bg-[#CA3F2A] text-white md:px-[40px] md:mt-8 sm:px-[30px] sm:mt-4 py-1 rounded-md md:text-lg sm:text-xs border-[#FFE3E3] border border-opacity-50   "
           >
            Check my Bookings
-          </button>
+          </a>
 
          
         </div>
